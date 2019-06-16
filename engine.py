282fb228f8d5ad94bb64b5377064be1f1181a384
Tyler Standridge <tstand90@gmail.com>
2019-06-16 09:39:53 -0700

    Possible end of part 5

--- engine.py
+++ engine.py
@@ -1,73 +1,8 @@
-# PART 1
-# import tcod
-# import tcod.event
-#
-# from input_handlers import handle_keys
-#
-#
-# def main():
-#     screen_width = 80
-#     screen_height = 50
-#
-#     player_x = int(screen_width / 2)
-#     player_y = int(screen_height / 2)
-#
-#     # Setup the font.
-#     tcod.console_set_custom_font(
-#         "arial10x10.png",
-#         tcod.FONT_LAYOUT_TCOD | tcod.FONT_TYPE_GREYSCALE,
-#     )
-#
-#     # Initialize the root console in a context.
-#     with tcod.console_init_root(
-#         w=screen_width,
-#         h=screen_height,
-#         order="F",
-#         renderer=tcod.RENDERER_SDL2,
-#         vsync=True
-#     ) as root_console:
-#         while True:
-#             # Display a basic test message
-#             # root_console.print(x=0, y=0, string='Hello World!', fg=tcod.white)
-#             root_console.print(x=player_x, y=player_y, string='@', fg=tcod.white)
-#
-#             # Show the console
-#             tcod.console_flush()
-#
-#             # Clear the console, so that when the characters move, they aren't redrawn in their original positions.
-#             root_console.clear()
-#
-#             for event in tcod.event.wait():
-#                 if event.type == 'QUIT':
-#                     raise SystemExit()
-#
-#                 if event.type == 'KEYDOWN':
-#                     action = handle_keys(event.sym, event.mod)
-#
-#                     move = action.get('move')
-#                     full_screen = action.get('fullscreen')
-#                     escape = action.get('escape')
-#
-#                     if move:
-#                         dx, dy = move
-#                         player_x += dx
-#                         player_y += dy
-#
-#                     if full_screen:
-#                         tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
-#
-#                     if escape:
-#                         raise SystemExit()
-#
-#
-# if __name__ == '__main__':
-#     main()
-
-
 import tcod
 import tcod.event
 
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
+from game_states import GameStates
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import render_all
@@ -88,6 +23,8 @@ def main():
     fov_light_walls = True
     fov_radius = 10
 
+    max_monsters_per_room = 3
+
     colors = {
         'dark_wall': tcod.Color(0, 0, 100),
         'dark_ground': tcod.Color(50, 50, 150),
@@ -95,14 +32,16 @@ def main():
         'light_ground': tcod.Color(200, 180, 50)
     }
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', tcod.white)
-    npc = Entity(int(screen_width / 2), int(screen_height / 2), '@', tcod.yellow)
-    entities = [npc, player]
+    player = Entity(0, 0, '@', tcod.white, 'Player', blocks=True)
+    entities = [player]
 
     game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
+                      max_monsters_per_room)
 
     fov_recompute = True
+
+    game_state = GameStates.PLAYERS_TURN
 
     # Setup the font.
     tcod.console_set_custom_font(
@@ -144,16 +83,33 @@ def main():
                     full_screen = action.get('fullscreen')
                     escape = action.get('escape')
 
-                    if move:
+                    if move and game_state == GameStates.PLAYERS_TURN:
                         dx, dy = move
+                        destination_x = player.x + dx
+                        destination_y = player.y + dy
 
                         if not game_map.is_blocked(player.x + dx, player.y + dy):
-                            player.move(dx, dy)
+                            if not game_map.is_blocked(destination_x, destination_y):
+                                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
 
-                            fov_recompute = True
+                                if target:
+                                    print(f'You kick the {target.name} in the shins, much to its annoyance!')
+                                else:
+                                    player.move(dx, dy)
+
+                                    fov_recompute = True
+
+                                game_state = GameStates.ENEMY_TURN
 
                     if full_screen:
                         tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
+
+                    if game_state == GameStates.ENEMY_TURN:
+                        for entity in entities:
+                            if entity != player:
+                                print(f'The {entity.name} ponders the meaning of its existence.')
+
+                        game_state = GameStates.PLAYERS_TURN
 
                     if escape:
                         raise SystemExit()
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,7 +1,9 @@
 from random import randint
 
+import tcod
 from tcod.map import Map
 
+from entity import Entity
 from map_objects.rectangle import Rect
 
 
@@ -37,7 +39,8 @@ class GameMap(Map):
     def is_blocked(self, x, y):
         return not self.walkable[x, y]
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
+                 max_monsters_per_room):
         rooms = []
         num_rooms = 0
 
@@ -86,9 +89,29 @@ class GameMap(Map):
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
+                self.place_entities(new_room, entities, max_monsters_per_room)
+
                 # finally, append the new room to the list
                 rooms.append(new_room)
                 num_rooms += 1
+
+    @staticmethod
+    def place_entities(room, entities, max_monsters_per_room):
+        # Get a random number of monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', tcod.desaturated_green, 'Orc', blocks=True)
+                else:
+                    monster = Entity(x, y, 'T', tcod.darker_green, 'Troll', blocks=True)
+
+                entities.append(monster)
 
     def render(self, console, colors):
         """
